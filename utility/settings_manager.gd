73d5e5d65525a48c42e184144c456fa5de36c9c9
Jonathan Loy <jonrloy@gmail.com>
2023-11-05 15:27:01 -0500

    more settings

--- utility/settings_manager.gd
+++ utility/settings_manager.gd
@@ -1,13 +1,18 @@
 extends Node
 
+signal settings_reverted
+
 const SETTINGS_FILE_PATH = "res://settings.cfg"
 
 var config = ConfigFile.new()
 
 var settings = {
 	"audio" : {
+		"master_muted": false,
 		"master_volume": 0,
+		"music_muted": false,
 		"music_volume": 0,
+		"sfx_muted": false,
 		"sfx_volume": 0,
 	},
 	"gameplay": {
@@ -33,15 +38,16 @@ var settings = {
 }
 
 func _ready():
-	#save_settings()
+	revert_settings()
 	load_settings()
 	
-func save_settings():
+func revert_settings():
 	for section in settings.keys():
 		for key in settings[section]:
 			config.set_value(section, key, settings[section][key])
 			
 	config.save(SETTINGS_FILE_PATH)
+	emit_signal("settings_reverted")
 	
 func load_settings():
 	var error = config.load(SETTINGS_FILE_PATH)
@@ -56,14 +62,39 @@ func load_settings():
 func get_settings():
 	return settings
 	
-func get_master_volume():
-	return settings["audio"]["master_volume"]
+func get_audio_bus_muted(bus):
+	var audio_bus = "%s_muted" % bus.to_lower()
+	return settings["audio"][audio_bus]
+
+func set_audio_bus_muted(bus, value):
+	var key = "%s_muted" % bus.to_lower()
+	config.set_value("audio", key, value)
+	save()
+		
+func get_audio_bus_volume(bus):
+	var audio_bus = "%s_volume" % bus.to_lower()
+	return settings["audio"][audio_bus]
 	
-func get_music_volume():
-	return settings["audio"]["music_volume"]
+func set_audio_bus_volume(bus, value):
+	var key = "%s_volume" % bus.to_lower()
+	config.set_value("audio", key, value)
+	save()
 	
 func get_autobomb():
 	return settings["gameplay"]["autobomb"]
+	
+func set_autobomb(value):
+	config.set_value("gameplay", "autobomb", value)
+	save()
 
 func get_bullet_color():
 	return settings["visual"]["bullet_color"]
+
+func set_bullet_color(color):
+	config.set_value("visual", "bullet_color", color)
+	save()
+
+func save():
+	config.save(SETTINGS_FILE_PATH)
+
+	
